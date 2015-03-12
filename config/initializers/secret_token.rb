2cc77376a03e35fb3201e909250500b7de52affe
venkat <venkateswara.rao@kvpcorp.com>
2015-03-12 11:06:04 +0530

    first commit to teamwork

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teamwork::Application.config.secret_key_base = 'f2c69b3898cfaf6839a83395e2d8b06456323c3dd1c751293e12e204bd150764bb88759b6b92a4a64e6f830195ebc086ad92d1b32d4c183684cda9f839b36c14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Teamwork::Application.config.session_store :cookie_store, key: '_teamwork_session'
